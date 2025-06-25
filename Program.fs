079f222f5f063d65dee0b9b41dae3a24ce03aa21
hediinn <hediinn@gmail.com>
2025-06-25 22:43:24 +0100

    f# er intrisant

--- Program.fs
+++ Program.fs
@@ -9,12 +9,27 @@ let filename = Path.GetFileName("./Industrial_Society.txt")
 printfn $"{filename}, {dirname}\n"
 printfn "Hello from F#"
 let mutable leb = []
-let rx = Regex(@"^\d+\. ",RegexOptions.Compiled)
+let rx = Regex(@"^\d+(\.|,) \w",RegexOptions.Compiled)
+let rx2 = Regex(@"",RegexOptions.Compiled)
 let lines = File.ReadLines(filename)
-let a = 0
-for i in lines do 
-    if rx.IsMatch(i) then printfn "lol"
-    else leb <- [i]|>  List.append leb
+let mutable a = 0
+let mutable s:string = ""
+for i in lines do
+    if rx.IsMatch(i) 
+    then 
+        s<-s + i 
+        printfn $"-- {i[0..10]}"
+    elif String.length i <1 && s.Length > 0
+    then 
+        leb<- [s] |> List.append leb 
+        s <- ""
+    elif String.length i >2 && s.Length >0
+    then 
+        s<-s+i
+    else 
+        printfn "??"
+
+printfn $"{leb[leb.Length-3..leb.Length-1]}"
 //for i in leb do
 //    printfn $"{i}"
 //lines |> Seq.iter(fun x -> if rx.IsMatch(x) then leb@x) 
